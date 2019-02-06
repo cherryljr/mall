558234b6eafb6d23be21f10d38a687af724023a6
cherryljr <travelerljr@gmail.com>
2019-02-07 00:55:34 +0800

    User Login

---
+++ src/main/java/com/mall/controller/portal/UserController.java
@@ -0,0 +1,27 @@
+package com.mall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password, HttpSession session) {
+
+        return null;
+    }
+}
