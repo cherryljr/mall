b22338648f4e4b36d0d7bcf7fffa9a4bd8813ea5
cherryljr <travelerljr@gmail.com>
2019-03-04 23:27:10 +0800

    收货地址管理模块

---
+++ src/main/java/com/mall/controller/portal/ShippingController.java
@@ -0,0 +1,82 @@
+package com.mall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mall.common.Constant;
+import com.mall.common.ResponseCode;
+import com.mall.common.ServerResponse;
+import com.mall.pojo.Shipping;
+import com.mall.pojo.User;
+import com.mall.service.IShippingService;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+    @Autowired
+    private IShippingService iShippingService;
+
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(), shipping);
+    }
+
+    @RequestMapping("del.do")
+    @ResponseBody
+    public ServerResponse del(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        // 该方法注意处理横向越权
+        return iShippingService.del(user.getId(), shippingId);
+    }
+
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        // 该方法注意处理横向越权
+        return iShippingService.update(user.getId(), shipping);
+    }
+
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse<Shipping> select(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        // 该方法注意处理横向越权
+        return iShippingService.select(user.getId(), shippingId);
+    }
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    // 分页功能
+    public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                         HttpSession session) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(), pageNum, pageSize);
+    }
+
+}
